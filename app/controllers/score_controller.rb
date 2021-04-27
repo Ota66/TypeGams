fbf5eb98911043548624675df35185336c52c24d
Ota66 <mitu.xxo66@gmail.com>
2021-04-27 13:16:24 +0900

    scoreコントローラー作成

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -2,5 +2,20 @@ class ScoreController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @score = Score.new
   end
+
+  def new
+    
+  end
+
+  def create
+    Score.create(score_params)
+  end
+
+  private
+  def score_params
+    params.require(:score).permit(:point, :true, :miss, :correct_answer_rate).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "score#index"
   devise_for :users
-  
+  resources :scores, only: [:index, :new, :create]
 end
